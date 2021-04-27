abac45242b7ee1b2f06f55e8de8d686031c7040d
tombetthauser <tombetthauser@gmail.com>
2021-04-27 12:17:48 -0700

    Get click through all images cycle working

--- update_script.py
+++ update_script.py
@@ -22,16 +22,32 @@ class SiteBot:
     self.options.add_argument('--no-sandbox')
     self.driver = webdriver.Chrome(executable_path="./chromedriver.exe", options=self.options)
 
-  def visit_url(self, url):
+  def visit_url(self, url, wait=2):
     self.driver.get(url)
-    # self.driver.get("https://www.icloud.com/sharedalbum/#B0o5oqs3q7vYSt")
+    sleep(wait)
     print(self.driver.title)
 
-  def take_screenshot(self, wait=2):
-    sleep(wait)
-    self.driver.get_screenshot_as_file("screenshot.png")
+  def take_screenshot(self, file_name="screenshot.png"):
+    self.driver.get_screenshot_as_file(file_name)
+
+  def get_images(self):
+    # driver.find_elements_by_class_name()
+    images = self.driver.find_elements_by_class_name("x-stream-photo-group-blocks-container-view")
+    
+    for i in range(len(images)):
+      images[i].click()
+      sleep(2)
+      self.take_screenshot(f"screenshot_{i}.png")
+      self.driver.back()
+      sleep(2)
+      images = self.driver.find_elements_by_class_name("x-stream-photo-group-blocks-container-view")
+
+    # images = self.driver.find_elements_by_tag_name("p")
+    print(len(images))
+
 
 
 new_bot = SiteBot()
 new_bot.visit_url("https://www.icloud.com/sharedalbum/#B0o5oqs3q7vYSt")
-new_bot.take_screenshot()
+# new_bot.take_screenshot()
+new_bot.get_images()
