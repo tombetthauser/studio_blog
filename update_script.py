49a70e3a12b615fe0e0078d4a0631d5e30f337d5
tombetthauser <tombetthauser@gmail.com>
2021-04-27 11:45:08 -0700

    Get error free run no actions

--- update_script.py
+++ update_script.py
@@ -1,4 +1,4 @@
-from selenium import selenium
+from selenium import webdriver
 from time import sleep
 
 
@@ -6,19 +6,19 @@ class SiteBot:
   def __init__(self):
     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:86.0) Gecko/20100101 Firefox/86.0"
 
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    options.add_argument(f'user-agent={user_agent}')
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--allow-running-insecure-content')
-    options.add_argument("--disable-extensions")
-    options.add_argument("--proxy-server='direct://'")
-    options.add_argument("--proxy-bypass-list=*")
-    options.add_argument("--start-maximized")
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
+    self.options = webdriver.ChromeOptions()
+    self.options.headless = True
+    self.options.add_argument(f'user-agent={user_agent}')
+    self.options.add_argument("--window-size=1920,1080")
+    self.options.add_argument('--ignore-certificate-errors')
+    self.options.add_argument('--allow-running-insecure-content')
+    self.options.add_argument("--disable-extensions")
+    self.options.add_argument("--proxy-server='direct://'")
+    self.options.add_argument("--proxy-bypass-list=*")
+    self.options.add_argument("--start-maximized")
+    self.options.add_argument('--disable-gpu')
+    self.options.add_argument('--disable-dev-shm-usage')
+    self.options.add_argument('--no-sandbox')
+    self.driver = webdriver.Chrome(executable_path="./chromedriver.exe", options=self.options)
 
 SiteBot()
