39306689fad5e37bb0fbcbc4d44338df352e89e0
tombetthauser <tombetthauser@gmail.com>
2021-04-27 11:39:21 -0700

    Add gitignore and exe

---
+++ update_script.py
@@ -0,0 +1,24 @@
+from selenium import selenium
+from time import sleep
+
+
+class SiteBot:
+  def __init__(self):
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:86.0) Gecko/20100101 Firefox/86.0"
+
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    options.add_argument("--disable-extensions")
+    options.add_argument("--proxy-server='direct://'")
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--start-maximized")
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
+
+SiteBot()
