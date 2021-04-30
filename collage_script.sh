23c7dc64fc6be5e914f0386a417af9d497675777
tombetthauser <tombetthauser@gmail.com>
2021-04-29 23:34:21 -0700

    Get random overlay working

--- collage_script.sh
+++ collage_script.sh
@@ -1,6 +1,37 @@
-echo "Running collage script..."
-ffmpeg -i B.png -i A.png -filter_complex "[1]scale=iw/2:-1[b];[0:v][b] overlay" out1.png
-ffmpeg -i out1.png -vf eq=brightness=100:saturation=200:contrast=-2000 -c:a copy out2.png
+# echo $((1 + RANDOM % 1000))
+# identify -format '%w %h' A.png
+
+width_A=$(identify -format '%w' A.png)
+height_A=$(identify -format '%h' A.png)
+width_B=$(identify -format '%w' B.png)
+height_B=$(identify -format '%h' B.png)
+
+random_width_A1=$((1 + RANDOM % $width_A))
+random_height_A1=$((1 + RANDOM % $height_A))
+random_width_B1=$((1 + RANDOM % $width_B))
+random_height_B1=$((1 + RANDOM % $height_B))
+
+random_width_A2=$((1 + RANDOM % $width_A))
+random_height_A2=$((1 + RANDOM % $height_A))
+random_width_B2=$((1 + RANDOM % $width_B))
+random_height_B2=$((1 + RANDOM % $height_B))
+
+# version="0002"
+# expr $height + 0
+# expr $version + 1
+
+# ffmpeg -i B.png -i A.png -filter_complex "[1]scale=iw/2:-1[b];[0:v][b] overlay" out1.png
+# ffmpeg -i out1.png -vf eq=brightness=100:saturation=200:contrast=-2000 -c:a copy out2.png
+
+ffmpeg \
+      -i A.png \
+      -i B.png \
+      -filter_complex \
+       "[1:v] scale=$random_width_A1:$random_height_A1 [ovr1], 
+       [1:v] scale=$random_width_B1:$random_height_B1 [ovrl2], 
+       [0:v][ovr1] overlay=$random_width_A2:$random_height_A2 [temp1], 
+       [temp1][ovrl2] overlay=$random_width_B2:$random_height_B2" \
+       out1.png
 
 # first integer refers to an image index, seems like zero or one
 # second number decreases the size of the second overlayed image as it increases and accepts floats
