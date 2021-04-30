de637dbc5c2ff5682fe46ebb4ee0086a8b7fde76
tombetthauser <tombetthauser@gmail.com>
2021-04-30 00:27:04 -0700

    Get random contrast working

--- collage_script.sh
+++ collage_script.sh
@@ -24,7 +24,9 @@ random_height_B2=$((1 + RANDOM % $height_B))
 # expr $version + 1
 
 # ffmpeg -i B.png -i A.png -filter_complex "[1]scale=iw/2:-1[b];[0:v][b] overlay" out1.png
-# ffmpeg -i out1.png -vf eq=brightness=100:saturation=200:contrast=-2000 -c:a copy out2.png
+
+
+output_file="collages/$(date +%s).png"
 
 ffmpeg \
       -i "$random_image_A" \
@@ -34,7 +36,16 @@ ffmpeg \
        [1:v] scale=$random_width_B1:$random_height_B1 [ovrl2], 
        [0:v][ovr1] overlay=$random_width_A2:$random_height_A2 [temp1], 
        [temp1][ovrl2] overlay=$random_width_B2:$random_height_B2" \
-       out1.png
+       $output_file
+
+random_brightness=$((-2 + RANDOM % 2))
+random_saturation=$((-2 + RANDOM % 2))
+random_contrast=$((-2 + RANDOM % 2))
+
+random_decimal_A="$(($RANDOM%3-1)).$((0 + RANDOM % 25))"
+
+ffmpeg -i $output_file -vf eq=contrast=$random_decimal_A -c:a copy "collages/$(date +%s)-c.png"
+# ffmpeg -i $output_file -vf eq=brightness=$random_decimal:saturation=$random_decimal:contrast=$random_decimal_A -c:a copy "collages/$(date +%s)-c.png"
 
 # first integer refers to an image index, seems like zero or one
 # second number decreases the size of the second overlayed image as it increases and accepts floats
